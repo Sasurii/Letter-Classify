27a2e60892ddb9aa1394287a250ee6605f8a9e2b
Stefanie Winkelhake <s.winkelhake@gmx.de>
2018-12-14 12:11:42 +0100

    Test

--- Letter Classify/Program.cs
+++ Letter Classify/Program.cs
@@ -8,64 +8,90 @@ using System.Threading.Tasks;
 
 namespace Letter_Classify
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-        
             Console.WriteLine("Briefmaße eingeben");
             Console.WriteLine();
             Console.WriteLine("Länge in mm: ");
             string length = Console.ReadLine();
+            if(length == "")
+            {
+                Console.WriteLine("Ungültig!");
+                Console.ReadLine();
+                return;
+            }
             Console.WriteLine("Breite in mm: ");
             string width = Console.ReadLine();
-            Console.WriteLine("Höhe in mm: ");
+            if (width == "")
+            {
+                Console.WriteLine("Ungültig!");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("Höhe in mm: ") ;
             string height = Console.ReadLine();
+            if (height == "")
+            {
+                Console.WriteLine("Ungültig!");
+                Console.ReadLine();
+                return;
+            }
             Console.WriteLine("Gewicht in g: ");
             string weight = Console.ReadLine();
+            if (weight == "")
+            {
+                Console.WriteLine("Ungültig!");
+                Console.ReadLine();
+                return;
+            }
             int lengthvalue = Convert.ToInt32(length);
             int widthvalue = Convert.ToInt32(width);
             int heightvalue = Convert.ToInt32(height);
             int weightvalue = Convert.ToInt32(weight);
 
+            string output = Classifier(lengthvalue, widthvalue, heightvalue, weightvalue);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+
+        public static string Classifier(int lengthvalue, int widthvalue, int heightvalue, int weightvalue)
+        {
             if (lengthvalue >= 140 && lengthvalue <= 235 && widthvalue >= 90 && widthvalue <= 125 &&
                 heightvalue <= 5 && weightvalue <= 20)
             {
-                Console.WriteLine("Standardbrief");
+                return("Standardbrief");
             }
             else if (lengthvalue >= 100 && lengthvalue <= 235 && widthvalue >= 70 && widthvalue <= 125 &&
                 heightvalue <= 10 && weightvalue <= 50)
             {
-                Console.WriteLine("Kompaktbrief");
+                return("Kompaktbrief");
             }
             else if (lengthvalue >= 100 && lengthvalue <= 335 && widthvalue >= 70 && widthvalue <= 250 &&
                 heightvalue <= 20 && weightvalue <= 500)
             {
-                Console.WriteLine("Großbrief");
+                return("Großbrief");
             }
             else if (lengthvalue >= 100 && lengthvalue <= 335 && widthvalue >= 70 && widthvalue <= 250 &&
                 heightvalue <= 50 && weightvalue <= 1000)
             {
-                Console.WriteLine("Maxibrief");
+                return("Maxibrief");
             }
             else if (lengthvalue >= 100 && lengthvalue <= 600 && widthvalue >= 70 && widthvalue <= 300 &&
                 heightvalue <= 150 && weightvalue <= 2000)
             {
-                Console.WriteLine("Maxibrief Plus");
+                return("Maxibrief Plus");
             }
-            else if ((lengthvalue+widthvalue+heightvalue) == 900 && lengthvalue <= 600 && widthvalue <= 600 &&
+            else if ((lengthvalue + widthvalue + heightvalue) == 900 && lengthvalue <= 600 && widthvalue <= 600 &&
                 heightvalue <= 600 && weightvalue <= 2000)
             {
-                Console.WriteLine("Maxibrief Plus");
+                return("Maxibrief Plus");
             }
             else
             {
-                Console.WriteLine("Ungültig!");
+                return("Ungültig!");
             }
-            Console.ReadLine();
-
-
-
         }
     }
 }
---
+++ Test/Class1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using Letter_Classify;
+
+namespace Classifier.Test
+{
+    public class ClassifierTest
+    {
+        [Fact]
+        ///<summary>
+        ///Classify Letters based on entries, output must be "Standardbrief"
+        ///</summary>
+        void Classifier_Test_Standardbrief()
+        { 
+            //Arrange 
+            int length = 140;
+            int width = 90;
+            int height = 5;
+            int weight = 20;
+
+            //Act
+            string ergebnis = Program.Classifier(length, width, height, weight);
+
+            //Assert
+            ergebnis.Should().Be("Standardbrief");
+        }
+        [Fact]
+        ///<summary>
+        ///Classify Letters based on entries, output must be "Kompaktbrief"
+        ///</summary>
+        void Classifier_Test_Kompaktbrief()
+        {
+            //Arrange
+            int length = 200;
+            int width = 100;
+            int height = 10;
+            int weight = 40;
+
+            //Act
+            string ergebnis = Program.Classifier(length, width, height, weight);
+
+            //Assert
+            ergebnis.Should().Be("Kompaktbrief");
+        }
+        [Fact]
+        ///<summary>
+        ///Classify Letters based on entries, output must be "Großbrief"
+        ///</summary>
+        void Classifier_Test_Großbrief()
+        {
+            //Arrange
+            int length = 250;
+            int width = 250;
+            int height = 15;
+            int weight = 350;
+
+            //Act
+            string ergebnis = Program.Classifier(length, width, height, weight);
+
+            //Assert
+            ergebnis.Should().Be("Großbrief");
+        }
+        [Fact]
+        ///<summary>
+        ///Classify Letters based on entries, output must be "Maxibrief"
+        ///</summary>
+        void Classifier_Test_Maxibrief()
+        {
+            //Arrange
+            int length = 140;
+            int width = 100;
+            int height = 25;
+            int weight = 200;
+
+            //Act
+            string ergebnis = Program.Classifier(length, width, height, weight);
+
+            //Assert
+            ergebnis.Should().Be("Maxibrief");
+        }
+        [Fact]
+        ///<summary>
+        ///Classify Letters based on entries, output must be "Maxibrief Plus"
+        ///</summary>
+        void Classifier_Test_Maxibrief_Plus()
+        {
+            //Arrange
+            int length = 600;
+            int width = 300;
+            int height = 150;
+            int weight = 2000;
+
+            //Act
+            string ergebnis = Program.Classifier(length, width, height, weight);
+
+            //Assert
+            ergebnis.Should().Be("Maxibrief Plus");
+        }
+        [Fact]
+        ///<summary>
+        ///Classify Letters based on entries, output must be "Maxibrief Plus"
+        ///</summary>
+        void Classifier_Test_Maxibrief_Plus2()
+        {
+            //Arrange
+            int length = 300;
+            int width = 300;
+            int height = 300;
+            int weight = 1800;
+
+            //Act
+            string ergebnis = Program.Classifier(length, width, height, weight);
+
+            //Assert
+            ergebnis.Should().Be("Maxibrief Plus");
+        }
+    }
+}
